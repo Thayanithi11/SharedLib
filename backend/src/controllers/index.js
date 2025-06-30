09714eb5a4ffb789e93cde9802a7de57ae387ce9
Thayanithi <thayanithi452004@gmail.com>
2025-06-30 11:56:36 +0530

    bfg

--- backend/src/controllers/index.js
+++
@@ -1,8 +0,0 @@
-class Controller {
-    // Example method for handling a request
-    static getExample(req, res) {
-        res.send('This is an example response from the controller.');
-    }
-}
-
-module.exports = Controller;
